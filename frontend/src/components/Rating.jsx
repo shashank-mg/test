cd6c3a9dcabbcbd1fdd8e1c1d883311fadfa433b
shashank m.g. <me30051997@outlook.com>
2025-10-09 05:55:02 +0530

    added rating page, custom bootstrap css, and index.css

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,28 @@
+import {Fa, FaStar, FaStarHalfAlt, FaRegStar} from 'react-icons/fa'
+
+const Rating = ({value, text, color}) => {
+    return (
+    <div className='rating'>
+        <span>
+            {value >= 1 ? <FaStar color={color} /> : value >= 0.5 ? <FaStarHalfAlt color={color} /> : <FaRegStar color={color} />}      
+        </span>
+        <span>
+            {value >= 2 ? <FaStar color={color} /> : value >= 1.5 ? <FaStarHalfAlt color={color} /> : <FaRegStar color={color} />}  
+        </span>
+        <span>
+            {value >= 3 ? <FaStar color={color} /> : value >= 2.5 ? <FaStarHalfAlt color={color} /> : <FaRegStar color={color} />}  
+        </span>
+        <span>
+            {value >= 4 ? <FaStar color={color} /> : value >= 3.5 ? <FaStarHalfAlt color={color} /> : <FaRegStar color={color} />}
+        </span>
+        <span>
+            {value >= 5 ? <FaStar color={color} /> : value >= 4.5 ? <FaStarHalfAlt color={color} /> : <FaRegStar color={color} />}  
+        </span>
+        <span>{text && text}</span>
+    </div>
+  )
+}
+
+
+
+export default Rating
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+import './assests/index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} >
+      <Route path="/" index={true} element={<HomeScreen />} />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-reportWebVitals();
