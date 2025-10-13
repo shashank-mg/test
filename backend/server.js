5fdaa88d91da0ababcd69b3371e599babe4fb68d
shashank m.g. <me30051997@outlook.com>
2025-10-13 09:49:06 +0530

    added connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
 const app = express();
-
-const PORT = process.env.PORT || 5000;
+connectDB();
+const PORT = 5000;
 
 app.get('/', (req, res) => {
   res.send('API is running...');
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from './components/Header'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
