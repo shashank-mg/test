40bbdf3304a978ce691d05453d0217bf3118ab1b
shashank m.g. <me30051997@outlook.com>
2025-10-17 22:15:48 +0530

    made changes to routes, added error handler

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,20 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import connectDB from './config/db.js';   
+import productRoutes from './routes/productRoutes.js';
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 const app = express();
 connectDB();
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+app.get('/', (req, res) => res.send('API is running...'));
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
